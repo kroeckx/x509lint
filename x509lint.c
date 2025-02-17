00a76c8bc557a91b5bb0773fed3ad9d22a3e5dbf
usowners <ownercod9@gmail.com>
2025-02-17 17:37:39 -0500

    x

--- x509lint.c
+++ x509lint.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Kurt Roeckx <kurt@roeckx.be>
+ * Copyright (c) 2025 McCoy L Stevens
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
